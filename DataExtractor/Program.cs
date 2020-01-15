9ca8a57c4ebfb95c070acafcc95db1f8c111f911
dantes1844 <dantes1844@hotmail.com>
2020-01-15 09:37:52 +0800

    查找一个极小数字的t，证明t不一定是大于0的

--- DataExtractor/Program.cs
+++ DataExtractor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace DataExtractor
@@ -13,7 +14,9 @@ namespace DataExtractor
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
-            ParallelRun();
+            //ParallelRun();
+
+            FindOne();
 
             stopwatch.Stop();
 
@@ -75,5 +78,44 @@ namespace DataExtractor
         }
 
         private static readonly ConcurrentStack<InputData> RecordStack = new ConcurrentStack<InputData>();
+
+        /// <summary>
+        /// 为了查找特殊数字的，结果证明t不一定是大于0的
+        /// </summary>
+        static void FindOne()
+        {
+            int index = 1;
+
+            var s1 = -1.33;
+            var s2 = -132;
+            var s3 = 133;
+            var randP = 165.03;
+            var dr = 3.3955;
+            var fenmu = s3 * dr - s1;
+            var item = IncreaseNumber;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var x = 35 + item;
+            while (true)
+            {
+                var t = randP * (1 - x * dr) / fenmu;
+                var ph1 = s1 * t + randP;
+                var ph2 = s2 * t + randP * (2 - x);
+                var pv = s3 * t + randP * x;
+
+                var flag1 = ph1 > ph2 + 2 && ph2 > pv + 2;
+                var flag2 = Math.Abs(ph1 / pv - dr) <= 0.0001;
+
+                if (flag2 && flag1)
+                {
+                    stopwatch.Stop();
+                    Debug.WriteLine($"成功:{index++},执行完一条耗时:{stopwatch.ElapsedMilliseconds}ms。符合条件：S1={s1:F4},S2={s2:F4},S3={s3:F4},RandP={randP:F4},Dr={dr:F4},X={x:F4},T={t:F4},Ph1={ph1:F4},Ph2={ph2:F4},Pv={pv:F4},S3*Dr-S1={fenmu:F4}");
+                    break;
+                }
+
+                Console.WriteLine($"{index++},X={x:F4},T={t:F4},Ph1={ph1:F4},Ph2={ph2:F4},Pv={pv:F4},Ph1-Ph2={ph1 - ph2:F4}");
+                x += item;
+            }
+        }
     }
 }
