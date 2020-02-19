7e3fbdf43ca1040a0199c9cf564d60dcd3d2b29d
edmonddantes <53289876+dantes1844@users.noreply.github.com>
2020-02-19 16:04:10 +0800

    增加深拷贝代码

---
+++ DesignPatterns/Prototype/DeepCopyPrototype/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DeepCopyPrototype
+{
+    public class Product : ICloneable
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+
+        public Address Address { get; set; }
+
+        public object Clone()
+        {
+            var address = (Address)Address.Clone();
+            var product = (Product)MemberwiseClone();
+            product.Address = address;
+            return product;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}-{Age}-{Address}";
+        }
+    }
+
+    public class Address : ICloneable
+    {
+        public string Province { get; set; }
+
+        public string City { get; set; }
+
+        public string District { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Province}-{City}-{District}";
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/DeepCopyPrototype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DeepCopyPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product()
+            {
+                Name = "充电宝",
+                Age = 1,
+                Address = new Address()
+                {
+                    Province = "湖北",
+                    City = "武汉",
+                    District = "洪山区"
+                }
+            };
+            Product p2 = (Product)p1.Clone();
+            p2.Age = 2; //值复制
+            p2.Name = "移动电源"; //值复制
+            p2.Address.District = "江夏区";//引用的地址，修改之后原来的对象也跟随变化
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.ReadKey();
+        }
+    }
+}
