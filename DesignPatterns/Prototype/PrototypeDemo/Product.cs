185ee9d8ea3ccd06b22e041ec538e444484f66d8
edmonddantes <53289876+dantes1844@users.noreply.github.com>
2020-02-19 12:44:57 +0800

    浅复制的原型模式

---
+++ DesignPatterns/Prototype/PrototypeDemo/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDemo
+{
+    public class Product : ICloneable
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+
+        public Address Address { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}-{Age}-{Address}";
+        }
+    }
+
+    public class Address
+    {
+        public string Province { get; set; }
+
+        public string City { get; set; }
+
+        public string District { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Province}-{City}-{District}";
+        }
+    }
+}
