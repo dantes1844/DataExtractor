e60c4d6d5806ad243286c4a97a8d70ff16c29d3c
edmonddantes <53289876+dantes1844@users.noreply.github.com>
2020-03-31 12:24:56 +0800

    增加书籍显示

--- DesignPatterns/Composition/Composition/Program.cs
+++ DesignPatterns/Composition/Composition/Program.cs
@@ -6,20 +6,33 @@ namespace Composition
     {
         static void Main(string[] args)
         {
-            Component root = new Composite("根节点");
+            //Component root = new Composite("根节点");
 
-            Component branch = new Composite("分支1");
-            branch.Add(new Leaf("分支1的叶子1"));
-            branch.Add(new Leaf("分支1的叶子2"));
+            //Component branch = new Composite("分支1");
+            //branch.Add(new Leaf("分支1的叶子1"));
+            //branch.Add(new Leaf("分支1的叶子2"));
 
-            var branch1 = new Composite("分支2");
-            branch1.Add(new Leaf("叶子1"));
-            branch1.Add(new Leaf("叶子2"));
+            //var branch1 = new Composite("分支2");
+            //branch1.Add(new Leaf("叶子1"));
+            //branch1.Add(new Leaf("叶子2"));
 
-            root.Add(branch);
-            root.Add(branch1);
+            //root.Add(branch);
+            //root.Add(branch1);
 
-            root.Display(2);
+            //root.Display(2);
+
+            Leaf leaf = new Leaf("yezi");
+
+            Leaf leaf2 = leaf;
+            leaf2 = new Leaf("yezi");
+
+            string a = "hello";
+            string b = "hello";
+            Console.WriteLine($"a=b={a == b}");
+            Console.WriteLine($"a.Equals(b)={a.Equals(b)}");
+
+            Console.WriteLine($"leaf2.Equals(leaf)={leaf2.Equals(leaf)}");
+            Console.WriteLine($"leaf2 == leaf={leaf2 == leaf}");
 
             Console.ReadKey();
 
---
+++ EBooksSettler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace EBooksSettler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderPath = ConfigurationManager.AppSettings["FolderPath"];
+
+
+            var directories = Directory.GetDirectories(folderPath);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var directory in directories)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+                sb.AppendFormat($"{directoryInfo.Name}{Environment.NewLine}");
+
+                var files = Directory.GetFiles(directory);
+
+                foreach (var file in files)
+                {
+                    if (file.EndsWith(".pdf"))
+                    {
+                        var fileInfo = new FileInfo(file);
+                        sb.AppendFormat($"\t{fileInfo.Name}{Environment.NewLine}");
+
+                    }
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+    }
+}
